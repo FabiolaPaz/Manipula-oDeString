8d9c9b522b2cf7b96ab40b3ed0d5e90f2f6d1c56
Fabiola Paz <fabiihpaaz@gmail.com>
2022-08-10 21:40:12 -0300

    Manipulando String

---
+++ index.js
@@ -0,0 +1,32 @@
+function upperCase(bianca) {
+    console.log(bianca.toUpperCase())
+}
+
+function toLowerCase(escola) {
+    console.log(escola.toLowerCase())
+}
+
+function trim(relogio) {
+    console.log(relogio.trim())
+}
+
+let teste1 = "Testando a função upperCase"
+console.log(teste1)
+console.log("")
+
+let teste2 = "TESTANDO FUNÇÃO TOLOWERCASE"
+console.log(teste2)
+console.log("")
+
+let teste3 = "   Testando a função trim   "
+console.log(teste3)
+console.log("")
+console.log("..........................")
+console.log("Frases após o uso da função: ")
+
+upperCase(teste1);
+console.log("")
+toLowerCase(teste2);
+console.log("")
+trim(teste3);
+console.log("Fim do código!!!")
